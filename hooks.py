3453fd5d5fb58013e51ad8aa70ef45ad5b87153e
Drew Stimson <drewstimson@Drews-MacBook-Air.local>
2018-08-10 17:18:06 -0400

    adding injected html for archive viewing

--- hooks.py
+++ hooks.py
@@ -13,3 +13,33 @@ def inject_archive(context):
         return ''
 
     return render(request, 'archive_plugin/inject.html')
+
+def inject_edit_article(context):
+    """
+    Inject a form to be injected at the beginning of the submission process
+    Prompts user to specify whether submission is new or update to existing article
+    If update, asks whether major (substantive content updates) or minor (typos)
+    """
+    request = context.get('request')
+    plugin = models.Plugin.objects.get(name=plugin_settings.SHORT_NAME)
+
+    edit_archive_enabled = setting_handler.get_plugin_setting(plugin, 'edit_article_enabled', request.journal)
+
+    if not edit_archive_enabled.value:
+        return ''
+
+    return render(request, 'archive_plugin/inject_edit_article.html')
+
+
+def inject_article_archive(context):
+    """
+    Injects a menu for the user to select a previous version of an article
+    """ 
+    return ''
+
+
+def inject_journal_archive(context):
+    """
+    Injects a menu for the user to select a previous archive of the encyclopedia
+    """
+    return ''
--- models.py
+++ models.py
@@ -2,3 +2,16 @@
 
 # probably need to track article version with the original article_id, keep dates of submission
 # seems like we need an entry for each archive of journal as well
+
+from django.db import models
+from submission.models import Article
+
+UPDATE_TYPES = (('m', 'Minor'), ('M', 'Major'))
+
+class ArticleUpdate(models.Model):
+    article_id = models.OneToOneField(Article, on_delete=models.CASCADE, primary_key=True)
+    prev_version = models.OneToOneField(Article, on_delete=models.CASCADE)
+    orig_article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    update_type = models.CharField(max_length=20, choices=UPDATE_TYPES)
+    new_author = models.BooleanField()
+    revision_date = models.DateTimeField(blank=True, null=True)
