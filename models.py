1ca36228300a06abff91f2626efed7b03353ac70
Drew Stimson <drewstimson@Drews-MacBook-Air.local>
2018-08-15 16:25:13 -0400

    updating model for tracking versions

--- models.py
+++ models.py
@@ -6,11 +6,11 @@
 from django.db import models
 from submission.models import Article
 
-UPDATE_TYPES = (('m', 'Minor'), ('M', 'Major'))
-
 class Version(models.Model):
-    article_id = models.OneToOneField(Article, on_delete=models.CASCADE, primary_key=True)
+    article_id = models.OneToOneField(Article, on_delete=models.CASCADE)
     orig_article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    update_type = models.CharField(max_length=20, choices=UPDATE_TYPES)
-    new_author = models.BooleanField()
+    update_type = models.CharField(max_length=20)
+    new_author = models.BooleanField(default=False)
     revision_date = models.DateTimeField(blank=True, null=True)
+
+# will having multiple relationships of different types (one-to-one and many-to-one) to submission.Article work?
