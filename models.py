fad77b44ade97912221f5c6cd25f5d417d88726a
Drew Stimson <drewstimson@Drews-Air.fios-router.home>
2018-08-17 00:37:25 -0400

    add empty base_article property

--- models.py
+++ models.py
@@ -16,9 +16,16 @@ UPDATE_CHOICES = [
 
 class Version(models.Model):
     article = models.OneToOneField(Article, null=True, on_delete=models.SET_NULL)
-    base_article = models.ForeignKey(Article, null=True, on_delete=models.SET_NULL)
+    parent_article = models.ForeignKey(Article, null=True, on_delete=models.SET_NULL, related_name='updates')
     update_type = models.CharField(max_length=20, choices=UPDATE_CHOICES)
     new_author = models.BooleanField(default=False)
     revision_date = models.DateTimeField(blank=True, null=True)
+
+    @property
+    def base_article(self):
+        """
+        Follow parent articles all the way down to the original base_article
+        """
+        pass
 
 # will having multiple relationships of different types (one-to-one and many-to-one) to submission.Article work?
