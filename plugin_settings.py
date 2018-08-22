5866304d66029ab9de3933db5314d5bf20d0abe4
hackman104 <stimsond@umich.edu>
2018-08-22 11:05:57 -0400

    set initial message template

--- plugin_settings.py
+++ plugin_settings.py
@@ -6,6 +6,7 @@ SHORT_NAME = 'archive_plugin'
 MANAGER_URL = 'archive_index'
 
 from utils import models, setting_handler
+from journal.models import Journal
 
 def install():
     new_plugin, created = models.Plugin.objects.get_or_create(name=SHORT_NAME, version=VERSION, enabled=True)
@@ -29,14 +30,17 @@ def install():
                                                 description='Template for the email sent to authors when an editor requests an article be updated',
                                                 is_translatable=False)
 
-    # Where does I initially set the template for the email message?
+    
     message_text = """
-        <p>Dear {{ article.owner.full_name }},</p>
-        <p>The editorial board of <i>{{ request.journal.name }}</i> requests that an article for which you are marked as the owner, '{{ article.title }},' be updated. Please follow the link below to begin the submission process.</p>
+        <p>Dear {{ article.corresponsdence_author.full_name }},</p>
+        <p>The editorial board of <i>{{ article.journal.name }}</i> requests that the article, '{{ article.title }},' be updated. Please follow the link below to begin the submission process.</p>
         <p><a href="{{ request.journal_base_url }}{% url 'inject_edit_article_selector' article.pk %}">Submit your update.</a></p>
-        <p>Best,<br>Editorial Board, <i>{{ request.journal.code }}</i></p>
+        <p>Best,<br>Editorial Board, <i>{{ article.journal.code }}</i></p>
     """
-    setting_handler.save_plugin_setting(new_plugin, 'request_email_template', message_text, request.journal)
+
+    # set starting message template for each journal
+    for journal in Journal.objects.all():
+        setting_handler.save_plugin_setting(new_plugin, 'request_email_template', message_text, journal)
 
 
 def hook_registry():
