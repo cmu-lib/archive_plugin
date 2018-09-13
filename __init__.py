ed6a1f3b1cad802efb39d7d3b484b070216e8f52
Andy Byers <andy.byers@openlibhums.org>
2018-09-13 22:46:10 +0100

    Added a gitignore and set .pyc to be ignored.

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from events import logic as event_logic
-from plugins.archive_plugin import logic
-
-event_logic.Events.register_for_event(event_logic.Events.ON_AUTHOR_PUBLICATION, logic.register_update_time)
